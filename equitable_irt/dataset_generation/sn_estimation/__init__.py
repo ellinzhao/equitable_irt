44e8e6ba9e6f20f3db038ca4b809d43e821920e1
Ellin Zhao <ellinqzhao@gmail.com>
2023-10-25 10:32:35 -0700

    Catch error when surface normals model weights are not present

--- equitable_irt/dataset_generation/sn_estimation/__init__.py
+++ equitable_irt/dataset_generation/sn_estimation/__init__.py
@@ -7,8 +7,12 @@ from .resnet_unet import ResNetUNet
 
 path = os.path.join(os.getcwd(), 'equitable_irt',
                     'dataset_generation', 'sn_estimation', 'model.pth')
-SN_MODEL = ResNetUNet(n_class=3)
-SN_MODEL.load_state_dict(torch.load(path, map_location=torch.device('cpu')))
-SN_MODEL.eval()
+try:
+    SN_MODEL = ResNetUNet(n_class=3)
+    SN_MODEL.load_state_dict(torch.load(path, map_location=torch.device('cpu')))
+    SN_MODEL.eval()
+except FileNotFoundError:
+    SN_MODEL = None
+    print('Surface normals model not loaded')
 
 __all__ = ['SN_MODEL', 'ResNetUNet']
