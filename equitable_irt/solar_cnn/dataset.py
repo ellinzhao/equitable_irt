f6346622d1784a136f76b757f163e4b4f59db239
Ellin Zhao <ellinqzhao@gmail.com>
2023-10-25 15:31:28 -0700

    Load reference base image in torch dataset

--- equitable_irt/solar_cnn/dataset.py
+++ equitable_irt/solar_cnn/dataset.py
@@ -16,31 +16,35 @@ LOAD_ROIS = Infrared.LOAD_ROIS
 
 class SolarDataset(Dataset):
 
-    def __init__(self, dataset_dir, ids, transform=None, roi_transform=None, train=False):
+    def __init__(self, dataset_dir, ids, num_load=4 * 15, transform=None, train=False):
         self.dataset_dir = dataset_dir
         self.ids = ids
         self.transform = transform
-        self.roi_transform = roi_transform
         self.train = train
+        self.num_load = num_load
         self.loader = lambda path: load_im(path, raw2temp).astype(np.float32)
 
         labels_data = [self._load_sub_labels(sid) for sid in ids]
-        labels, ref_rois = list(zip(*labels_data))
+        labels, ref_rois, ref_ims = list(zip(*labels_data))
         self.labels = pd.concat(labels)
         self.ref_rois = {self.ids[i]: ref_rois[i] for i in range(len(self.ids))}
+        self.ref_ims = {self.ids[i]: ref_ims[i] for i in range(len(self.ids))}
 
     def _load_sub_labels(self, sid):
         fix_dir = lambda x: os.path.join(self.dataset_dir, sid, x)
-        base_df = pd.read_csv(fix_dir('base.csv')).iloc[:4 * 30]
-        cool_df = pd.read_csv(fix_dir('cool.csv')).iloc[:4 * 30]
+        base_df = pd.read_csv(fix_dir('base.csv')).iloc[:self.num_load]
+        cool_df = pd.read_csv(fix_dir('cool.csv')).iloc[:self.num_load]
         ref_roi_df = pd.read_csv(fix_dir('base_rois.csv'))
         ref_roi = np.array(ref_roi_df.iloc[0])[1:]
+
+        fname = fix_dir(base_df.iloc[4]['ir_fname'])
+        ref_im = load_im(fname, raw2temp)
 
         # Combine base and cool labels
         df = pd.concat([base_df, cool_df])
         df['ir_fname'] = df['ir_fname'].apply(fix_dir)
         df['rgb_fname'] = df['rgb_fname'].apply(fix_dir)
-        return df, ref_roi
+        return df, ref_roi, ref_im
 
     def load_bbox(self, row):
         pts = []
@@ -72,10 +76,11 @@ class SolarDataset(Dataset):
         fname = row['ir_fname']
         ir = self.loader(fname)
         ref_rois = self.ref_rois[os.path.basename(os.path.dirname(fname))]
+        ref_im = self.ref_ims[os.path.basename(os.path.dirname(fname))]
         mask = self.bbox_mask(bbox, ir.shape)
-
         if self.transform:
             ir = self.transform(ir)
-            ref_rois = self.roi_transform(ref_rois)
+            ref_im = self.transform(ref_im)
+            ref_rois = self.transform(ref_rois[:, None])
             mask = ToTensor()(mask)
-        return ir, ref_rois, mask, is_base
+        return ir, ref_rois, mask, ref_im, is_base
--- equitable_irt/solar_cnn/loss.py
+++ equitable_irt/solar_cnn/loss.py
@@ -6,15 +6,23 @@ class FaceFeatureLoss(nn.Module):
 
     def __init__(self):
         super(FaceFeatureLoss, self).__init__()
+        self.criterion1 = nn.MSELoss()
+        self.criterion2 = nn.MSELoss()
 
-    def forward(self, est_base, mask, base_rois):
+    def extract_rois(self, est_base, mask):
+        est_rois = torch.sum(est_base * mask, axis=(-2, -1))
+        est_rois = est_rois / torch.sum(mask, axis=(-2, -1))
+        return est_rois
+
+    def forward(self, est_base, mask, base_rois, tskin):
         # est_base:  b, 1, h, w
         # mask:      b, 10, h, w
         # base_rois: b, 10
-        criterion = nn.MSELoss()
-        est_rois = torch.sum(est_base * mask, axis=(-2, -1))
-        est_rois = est_rois / torch.sum(mask, axis=(-2, -1))
-        loss = criterion(est_rois, base_rois)
+        base_rois = base_rois.squeeze(-1)
+        base_rois = base_rois.squeeze(1)
+
+        est_rois = self.extract_rois(est_base, mask)
+        loss = self.criterion1(est_rois.float(), base_rois)
 
         # Should there be a loss involving Tskin or is_base
         return loss
