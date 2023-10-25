d5fa11d4efe15826a3c6ac59689ee47cbb355d40
Ellin Zhao <ellinqzhao@gmail.com>
2023-10-25 15:34:52 -0700

    Fix missing UNet import

--- equitable_irt/solar_cnn/__init__.py
+++ equitable_irt/solar_cnn/__init__.py
@@ -1,6 +1,7 @@
 from .dataset import SolarDataset
 from .loss import FaceFeatureLoss
 from .models import ConvAutoencoder
+from .models import UNet
 
 
-__all__ = ['ConvAutoencoder', 'FaceFeatureLoss', 'SolarDataset']
+__all__ = ['ConvAutoencoder', 'FaceFeatureLoss', 'SolarDataset', 'UNet']
