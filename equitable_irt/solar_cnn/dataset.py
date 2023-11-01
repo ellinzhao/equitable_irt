8b294a380b4e080fd8f505f4439291d2e89125f8
Ellin Zhao <ellinqzhao@gmail.com>
2023-11-01 11:05:36 -0700

    Load RGB images along with IR images for the dataset

--- equitable_irt/solar_cnn/dataset.py
+++ equitable_irt/solar_cnn/dataset.py
@@ -1,28 +1,30 @@
 import os
+import random
 
+import cv2
 import numpy as np
 import pandas as pd
 import torch
 from torch.utils.data import Dataset
-from torchvision.transforms import ToTensor
 
 from ..utils import load_im
 from ..utils import raw2temp
-from ..dataset_generation import Infrared
 
-
-LOAD_ROIS = Infrared.LOAD_ROIS
+bgr2gray = lambda im: cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
 
 class SolarDataset(Dataset):
 
-    def __init__(self, dataset_dir, ids, num_load=4 * 15, transform=None, train=False):
+    def __init__(self, dataset_dir, ids, num_load=60, transform=None, ir_transform=None, train=False):
         self.dataset_dir = dataset_dir
         self.ids = ids
         self.transform = transform
+        self.ir_transform = ir_transform
         self.train = train
         self.num_load = num_load
+
         self.loader = lambda path: load_im(path, raw2temp).astype(np.float32)
+        self.gray_loader = lambda path: load_im(path, bgr2gray).astype(np.float32)
         self.labels = pd.concat([self._load_sub_labels(sid) for sid in ids])
 
     def _load_sub_labels(self, sid):
@@ -48,9 +50,21 @@ class SolarDataset(Dataset):
         base_ir = self.loader(base_ir_fname)
         session_type = [base_ir_fname == ir_fname, (base_ir_fname != ir_fname)]
 
+        rgb_fname = row['rgb_fname']
+        rgb_fname = row['rgb_fname'].replace('.png', '.jpg')  # TODO(ellin): fix this.
+        gray = self.gray_loader(rgb_fname)
+
+        if self.train and session_type[0] and random.uniform(0, 1) > 0.5:
+            # For baseline images, add temp offset to mimic fever
+            offset = random.uniform(2, 4)
+            ir += offset
+            base_ir += offset
+
+        data_input = np.dstack([ir, gray])
+
         if self.transform:
-            ir = self.transform(ir)
-            base_ir = self.transform(base_ir)
+            data_input = self.transform(data_input)
+            base_ir = self.ir_transform(base_ir)
             session_type = torch.tensor(session_type)
 
-        return ir, base_ir, session_type
+        return data_input, base_ir, session_type
